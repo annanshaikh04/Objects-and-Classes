72c61274042720bfc1afab07907874781a7d9212
annanshaikh04 <81747373+annanshaikh04@users.noreply.github.com>
2021-04-18 15:40:36 +0530

    Created ClassandObject.java
    This file do square root of 625.

---
+++ ClassandObject.java
@@ -0,0 +1,11 @@
+class ClassandObject{
+    public static void main(){
+        int a=625;
+        System.out.println("Square root of"+a+"is : " +Math.sqrt(a));
+        myMethod();
+    }
+    public static void myMethod(){
+        System.out.println("User define method is called");
+    }
+}
+        
